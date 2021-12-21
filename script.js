27f9d62ba464fde2c8c0512f5659d9d63f9d5b8c
Jéssica Florentino <jdgflorentino@gmail.com>
2021-12-21 16:32:25 -0300

    Requisitos 10 e 11.

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function pixelBoard() {
     }
 }
 
-pixelBoard();
+pixelBoard(25);
 
 function colorSelected() {
     let elementColor = document.getElementsByClassName('color');
@@ -58,31 +58,84 @@ function pixelSelected() {
 pixelSelected();
 
 function cleanBoard() {
-
     let main = document.querySelector('#main');
     let firstSection = document.querySelector('#color-palette');
     let sectionButton = document.createElement('section');
     main.insertBefore(sectionButton, firstSection.nextSibling);
-    sectionButton.id = 'section-button'
+    sectionButton.id = 'section-button';
 
     let button = document.createElement('button');
     button.id = 'clear-board';
     button.innerText = 'Limpar';
     sectionButton.appendChild(button);
 
-
     button.addEventListener('click', function() {
-
         let pixels = document.getElementsByClassName('pixel');
 
         for (let i = 0; i < pixels.length; i += 1) {
             pixels[i].style.backgroundColor = 'white';
-
         }
-
-    })
-
-
-};
+    });
+}
 
 cleanBoard();
+
+// Requisitos Bônus
+
+let sectionButton = document.querySelector('#section-button');
+let inputBoard = document.createElement('input');
+let buttonBoard = document.createElement('button');
+inputBoard.id = 'board-size';
+inputBoard.type = 'number';
+inputBoard.min = '1';
+buttonBoard.id = 'generate-board';
+buttonBoard.innerText = 'VQV';
+sectionButton.appendChild(inputBoard);
+sectionButton.appendChild(buttonBoard);
+
+function boardRemoved() {
+    let buttonBoard = document.querySelector('#generate-board');
+
+    buttonBoard.addEventListener('click', function() {
+        let board = document.getElementById('pixel-board');
+        board.parentNode.removeChild(board);
+
+    });
+}
+
+boardRemoved();
+
+function limite() {
+    let main = document.getElementById('main');
+    let board = document.createElement('section');
+    board.id = 'pixel-board';
+    main.appendChild(board);
+    let inputBoard = document.getElementById('board-size');
+    let buttonBoard = document.getElementById('generate-board');
+
+    buttonBoard.addEventListener('click', function() {
+
+        if (inputBoard.value > 0) {
+            if (inputBoard.value < 5) {
+                inputBoard.value = 5;
+            } else if (inputBoard.value > 50) {
+                inputBoard.value = 50;
+            }
+        } else {
+            alert('Board inválido!');
+        }
+
+        let width = inputBoard.value * 42;
+        board.style.width = width + 'px';
+        let num = inputBoard.value ** 2;
+
+        for (let i = 0; i < num; i += 1) {
+            let elementDiv = document.createElement('div');
+            elementDiv.className = 'pixel';
+            board.appendChild(elementDiv);
+            inputBoard.value = '';
+        }
+    });
+}
+
+limite();
